5d4475138d7141a3c5431c0497263be0fcf9cd9f
Govind Rai <govindraithanwani@gmail.com>
2025-01-26 22:08:47 +0500

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,28 @@
+class Task2{
+	public static void main(String[] args){
+	boolean Equal=(10==15);
+	boolean NotEqual=(20!=25);
+	boolean Greater=(30>25);
+	boolean Less=(12<18);
+	boolean GreaterORequal=(40>=40);
+	boolean LessORequal=(7<=10);
+	String hello = "hello";
+	String world = "world";
+	String apple = "apple";
+	String orange = "orange";
+
+	boolean HelloEqual = (hello==world);
+	boolean AppleNotEqual = (apple!=orange);
+
+	System.out.println("Equal =" +Equal);
+	System.out.println("NotEqual =" +NotEqual);
+	System.out.println("Greater =" +Greater);
+	System.out.println("Less =" +Less);
+	System.out.println("GreaterORequal =" +GreaterORequal);
+	System.out.println("LessORequal =" +LessORequal);
+	System.out.println("HelloEqual = " + HelloEqual);
+	System.out.println("AppleNotEqual = " + AppleNotEqual);
+
+	
+	}
+}
